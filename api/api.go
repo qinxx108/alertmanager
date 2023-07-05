f1a725466d3933ffd3ae0c3c0ba89369f80d450e
Yijie Qin <qinyijie@amazon.com>
2023-07-05 18:00:31 -0400

    add the signature
    Signed-off-by: Yijie Qin <qinyijie@amazon.com>

--- api/api.go
+++ api/api.go
@@ -76,7 +76,9 @@ type Options struct {
 	// according to the current active configuration. Alerts returned are
 	// filtered by the arguments provided to the function.
 	GroupFunc func(func(*dispatch.Route) bool, func(*types.Alert, time.Time) bool) (dispatch.AlertGroups, map[model.Fingerprint][]string)
-
+	// GroupInfoFunc returns a list of alert groups information. The alerts are grouped
+	// according to the current active configuration. This function will not return the alerts inside each group.
+	GroupInfoFunc func(func(*dispatch.Route) bool) dispatch.AlertGroupInfos
 	// APICallback define the callback function that each api call will perform before returned.
 	APICallback callback.Callback
 }
